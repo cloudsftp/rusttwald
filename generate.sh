a1f783561c8377f909b5f3d12f747444ca07d2a2
Fabian Weik <fabian.weik@protonmail.com>
2024-08-24 22:05:54 +0200

    add useful commands to generate script as comments

--- generate.sh
+++ generate.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
+# get api spec:
+# curl https://api.mittwald.de/v2/openapi.json > original-api.json
+
+# filter api spec:
+# https://github.com/Mermade/openapi-filter
+
+# get openapi-generator-cli:
+# wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.7.0/openapi-generator-cli-7.7.0.jar -O openapi-generator-cli.jar
+
 java -jar openapi-generator-cli.jar \
     generate \
     -g rust \
